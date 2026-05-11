d3a1293613a037d05c6eb47d1d5ed5dc471c860a
Vadim <aonyko@gmail.com>
2026-05-11 13:57:21 +0300

    add Map

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -32,8 +30,38 @@ public class Main {
         names.sort((a, b) -> a.compareTo(b));
         System.out.println(names);
 
-        names.sort((a,b)-> a.length()-b.length());
+        names.sort((a, b) -> a.length() - b.length());
         System.out.println(names);
+
+        Map<String, Integer> players = new HashMap<>();
+
+        players.put("Max", 120);
+        players.put("Ivan", 90);
+        players.put("Oleg", 150);
+        players.put("Anna", 110);
+        players.put("Mia", 200);
+
+        Set<Map.Entry<String, Integer>> entries = players.entrySet();
+        System.out.println("Set entry map players : " + entries);
+
+        List<Map.Entry<String, Integer>> playersList = new ArrayList<>(players.entrySet());
+        System.out.println(playersList);
+//        String bestScoresName = null;
+//        int maxCount = 0;
+//        for (Map.Entry<String, Integer> entry : players.entrySet()) {
+//            if (entry.getValue() > maxCount) {
+//                maxCount = entry.getValue();
+//                bestScoresName = entry.getKey();
+//            }
+//        }
+//        System.out.println(bestScoresName + " : "  + maxCount);    cкажу откровенно взял этот метод с соседнего урока , вспомнил что есть сам не писал
+        playersList.sort((a, b) -> a.getValue().compareTo(b.getValue()));
+        System.out.println("Best Player is " + playersList.getLast().getKey() + " : "
+                + playersList.getLast().getValue() + "points");
+
+        // сортировка выше
+        System.out.println(playersList);
+        playersList.sort((a, b) -> a.getKey().compareTo(b.getKey()));
 
 
     }
